7724854754d7452f99790eef26a9f1f3072d0610
Frank Taillandier <frank@forestry.io>
2021-05-13 16:08:12 +0200

    chore: Next.js Preview

--- pages/admin/posts/[slug].js
+++ pages/admin/posts/[slug].js
@@ -1,5 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useGraphqlForms } from 'tina-graphql-gateway'
+import { useRouter } from 'next/router'
+import Post from '../../posts/[slug]'
+export default function BlogPostEditor() {
+  const query = gql => gql`
+    query BlogPostQuery($relativePath: String!) {
+      getPostsDocument(relativePath: $relativePath) {
+        data {
+          __typename
+          ... on Post_Doc_Data {
+            title
+            excerpt
+            coverImage
+            date
+            author {
+              name
+              picture
+            }
+            ogImage {
+              url
+            }
+            _body
+          }
+        }
+      }
+    }
+  `
 
-export default function() {
-  return <div>My admin page</div>
+  const router = useRouter()
+  const [payload, isLoading] = useGraphqlForms({
+    query,
+    variables: { relativePath: `${router.query.slug}.md` },
+    formify: ({ createForm, formConfig }) => {
+      formConfig.fields?.forEach(field => {
+        //use markdown plugin with _body field
+        if (field.name === '_body') {
+          field.component = 'markdown'
+        }
+      })
+      return createForm(formConfig)
+    },
+  })
+
+  const [content, setContent] = useState('')
+  useEffect(() => {
+    const parseMarkdown = async () => {
+      let { _body } = payload?.getPostsDocument.data || ''
+      setContent(await markdownToHtml(_body))
+    }
+
+    parseMarkdown()
+  }, [payload?.getPostsDocument.data._body])
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  let { _body, ...post } = payload.getPostsDocument.data
+  post.slug = router.query.slug
+  post.content = content
+  return <Post post={post} />
 }
